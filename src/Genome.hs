6040e87f040c9244196fb63bdb949273e78d8479
Joshua Bassett <hello@joshbassett.info>
2024-02-15 22:13:13 +1100

    Concat names in Genome.cross function

--- src/Genome.hs
+++ src/Genome.hs
@@ -16,4 +16,4 @@ buildGenome name genes = Genome {Genome.name = name, Genome.genes = genes}
 
 -- | Crosses two genomes together to produce a third genome.
 cross :: Genome -> Genome -> Genome
-cross a b = buildGenome (Genome.name a) (mix (genes a) (genes b))
+cross a b = buildGenome (Genome.name a ++ Genome.name b) (mix (genes a) (genes b))
